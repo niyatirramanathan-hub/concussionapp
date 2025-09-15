fb426cbb0894d57c3023070498f7218852cee4e3
niyatirramanathan-hub <niyati.r.ramanathan@gmail.com>
2025-09-14 17:19:20 -0700

    Update login.py

--- login.py
+++ login.py
@@ -9,8 +9,7 @@ Original file is located at
 
 import streamlit as st
 import authlib
-IMAGE_ADDRESS = "https://www.theglobeandmail.com/resizer/v2/ZEC33ARWYNF7JKHKH6NN2PLRCQ.jpg?auth=b96fceb4acb641eb7765f3f1dff182dfa52eba742065b9da99b560e1ea32da21&width=1200&height=800&quality=80&smart=true"
-
+IMAGE_ADDRESS = "concussion.jpeg"
 if not st.user.is_logged_in:
   st.title("Google Login")
   st.image(IMAGE_ADDRESS)
